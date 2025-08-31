83286234f3b092c4461ba2c0eeea2dedac2b2155
Your Name <you@example.com>
2025-08-31 23:35:16 +0530

    mia implemented

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ while True:
     response = client.models.generate_content(
         model="gemini-2.5-flash",
         contents=f"""user asked {user_command},
-        user have asked to do something and you have to answer the query 
+
+        user have asked to do something and you have to answer the query
         and if the user have asked to open something that time you just have to provide the commmand to run in the terminal of the {operating_system},
         and also make sure that if you are giving the command to run in terminal that time the response must not include any single markdown text.
         also if the user is commanding to do something in the system that time you need to just provide the terminal command in such a way that they get executed directly and this response should also not include any type of single markdown content
@@ -29,7 +30,12 @@ while True:
 
         if there is only result as response:
         "type":"response", "content":"content"
-        
+
+        now there is one more condition and that is
+        is the user prompt is not starting with the 'mia bhai' that time it should just say 'Mera name Mia bhai! ha to Mia bhai bhi use kro....' else it  must work normally
+        and the response must be in form:
+        "type":"response", "content":"content"
+
         """,
     )
 
@@ -38,7 +44,6 @@ while True:
     if result['type'] == 'command':
         try:
             os.system(result['content'])
-            
         except:
             print(result['fail_audio'])
         
